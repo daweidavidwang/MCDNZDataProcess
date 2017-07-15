5c967ba1cf3b91f5356f5bae1f28afc21d9a2b73
David Wang <David Wang>
2017-07-15 17:22:02 +0800

    ok

---
+++ VarienceRGBConvert.m
@@ -0,0 +1,19 @@
+folderGT = 'E:\datasetcombine\valuableGT';
+folderMat = 'F:\dataset1_var_train\64spp';
+folderExr = 'F:\dataset1_var_train\64spp\exr';
+filepathGt = [];
+ext               =  {'*.jpg'};
+for i = 1 : length(ext)
+    filepathGt = cat(1,filepathGt, dir(fullfile(folderGT, ext{i})));
+end
+
+for i = 1 : length(filepathGt)
+     image = imread(fullfile(folderGT,filepathGt(i).name));
+     input_im = load( char(fullfile(folderMat,[filepathGt(i).name(1:end-4),'.mat'] )) );
+     input_im = single(reshape(input_im.Var,[size(image,2) size(image,1) 18]));
+     input_im = permute( input_im(:,:,3:end), [2,1,3] );
+     luminance = 0.2126*input_im(:,:,1)+0.7952*input_im(:,:,2)+0.0722*input_im(:,:,3);
+     exrwrite(luminance,fullfile(folderExr,[filepathGt(i).name(1:end-4),'.exr']));
+end
+
+     
